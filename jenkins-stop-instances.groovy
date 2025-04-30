be609c9cf8f8753bafacdb045f83780736c55b19
Rafael Cardoso <raafa001@gmail.com>
2025-04-29 23:51:13 -0300

    Update jenkins-stop-instances.groovy

--- jenkins-stop-instances.groovy
+++ jenkins-stop-instances.groovy
@@ -1,12 +1,19 @@
 pipeline {
     agent any
     stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main',
+                    credentialsId: 'github-app',
+                    url: 'https://github.com/raafa001/spot-render.git'
+            }
+        }
         stage('Stop Instances') {
             steps {
                 script {
                     sh '''
-                    chmod +x ~/git/ci-cd-pipelines/scripts/stop-instances.sh
-                    ~/git/ci-cd-pipelines/scripts/stop-instances.sh
+                    chmod +x scripts/stop-instances.sh
+                    ./scripts/stop-instances.sh
                     '''
                 }
             }
