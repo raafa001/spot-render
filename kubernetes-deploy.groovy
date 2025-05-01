fe34ec8060e846b703340397ac9679280cffc921
Rafael Cardoso <raafa001@gmail.com>
2025-05-01 12:09:32 -0300

    Update kubernetes-deploy.groovy

--- kubernetes-deploy.groovy
+++ kubernetes-deploy.groovy
@@ -10,19 +10,19 @@ pipeline {
         }
         stage('Deploy Monitoring') {
             steps {
-                withAWS(credentials: 'spot-render', region: 'us-east-1') {
+                withAWS(credentials: 'aws-credentials', region: 'us-east-1') {
                     script {
-                        echo 'Applying Grafana and Prometheus manifests...'
+                        echo 'Applying Grafana and Prometheus manifests from kubernetes directory...'
                         sh 'kubectl create namespace monitoring --dry-run=client -o yaml | kubectl apply -f -'
-                        sh 'kubectl apply -f grafana-deployment.yaml -n monitoring'
-                        sh 'kubectl apply -f grafana-service.yaml -n monitoring'
-                        sh 'kubectl apply -f prometheus-deployment.yaml -n monitoring'
-                        sh 'kubectl apply -f prometheus-service.yaml -n monitoring'
-                        sh 'kubectl apply -f prometheus-configmap.yaml -n monitoring'
-                        sh 'kubectl apply -f prometheus-rules.yaml -n monitoring'
-                        sh 'kubectl apply -f grafana-datasource.yaml -n monitoring'
-                        sh 'kubectl apply -f grafana-dashboard-provider.yaml -n monitoring'
-                        sh 'kubectl apply -f grafana-dashboards.yaml -n monitoring'
+                        sh 'kubectl apply -f kubernetes/grafana-deployment.yaml -n monitoring'
+                        sh 'kubectl apply -f kubernetes/grafana-service.yaml -n monitoring'
+                        sh 'kubectl apply -f kubernetes/prometheus-deployment.yaml -n monitoring'
+                        sh 'kubectl apply -f kubernetes/prometheus-service.yaml -n monitoring'
+                        sh 'kubectl apply -f kubernetes/prometheus-configmap.yaml -n monitoring'
+                        sh 'kubectl apply -f kubernetes/prometheus-rules.yaml -n monitoring'
+                        sh 'kubectl apply -f kubernetes/grafana-datasource.yaml -n monitoring'
+                        sh 'kubectl apply -f kubernetes/grafana-dashboard-provider.yaml -n monitoring'
+                        sh 'kubectl apply -f kubernetes/grafana-dashboards.yaml -n monitoring'
                     }
                 }
             }
