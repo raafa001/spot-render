05a7a22df530eb652fd7871c42e6872fae1cd5b7
Rafael Cardoso <raafa001@gmail.com>
2025-05-01 11:39:39 -0300

    Update kubernetes-deploy.groovy

--- kubernetes-deploy.groovy
+++ kubernetes-deploy.groovy
@@ -1,10 +1,21 @@
 pipeline {
     agent any
     stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main',
+                    credentialsId: 'github-app', // Substitua pelo ID da sua credencial do GitHub
+                    url: 'https://github.com/seu-repositorio/seu-projeto.git' // Substitua pela URL do seu repositório
+            }
+        }
         stage('Deploy to Kubernetes') {
             steps {
-                sh 'kubectl apply -f deployment.yaml'
-                sh 'kubectl apply -f service.yaml'
+                withAWS(credentials: 'aws-credentials', region: 'sua-regiao-aws') { // Substitua pelo ID da sua credencial AWS e sua região
+                    script {
+                        sh 'kubectl apply -f deployment.yaml'
+                        sh 'kubectl apply -f service.yaml'
+                    }
+                }
             }
         }
     }
